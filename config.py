c96d81a87eb247bf77a325d62af899859c1d0d99
A Epstein <38995679+a-epstein@users.noreply.github.com>
2019-11-14 11:11:49 +0000

    Add check_same_thread=False to sqlite URI

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'app.db?check_same_thread=False')
     SQLALCHEMY_TRACK_MODIFICATIONS = False

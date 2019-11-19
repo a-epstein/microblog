1c7aca8426a9f6fef3d03ca5fd8409ea3508c7d0
A Epstein <38995679+a-epstein@users.noreply.github.com>
2019-11-19 13:56:12 +0000

    Userpage view function

--- app/routes.py
+++ app/routes.py
@@ -78,3 +78,15 @@ def register():
         flash("Congratulations, you are now a registered user!")
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+# add view function for user profile pages
+# the view function has a dynamic component as denoted by <username> so that Flask can fill in with logged in user
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Text post #1'},
+        {'author': user, 'body': 'Text post #2'}
+    ]
+    return render_template('user.html',user=user, posts=posts)

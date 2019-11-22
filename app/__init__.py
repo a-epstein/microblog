85ef2bd4cd6e747f73c07d87a5700c7fbc1faaef
A Epstein <38995679+a-epstein@users.noreply.github.com>
2019-11-22 11:55:35 +0000

    Error page handling

--- app/__init__.py
+++ app/__init__.py
@@ -15,17 +15,17 @@ login = LoginManager(app)
 # this is the endpoint (url) for logins
 login.login_view = 'login'
 
-if not app.debug():
+if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD']
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
         secure = None
         if app.config['MAIL_USE_TLS']:
             secure = ()
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='noreply@' + app.config['MAIL_SERVER'],
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
             toaddrs=app.config['ADMINS'], subject='Microblog Failure',
             credentials=auth,secure=secure)
         mail_handler.setLevel(logging.ERROR)
